0a7bee50a6e0ab50e5610db0580a4654ac8621ad
tahany777 <tahanymohammed777@hotmail.com>
2023-05-08 16:48:25 +0300

    edit

--- models/user.model.js
+++ models/user.model.js
@@ -1,43 +1,68 @@
-'use strict';
-const jwt = require('jsonwebtoken');
+"use strict";
+const jwt = require("jsonwebtoken");
 
 module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define('user', {
-        userName: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        email: {
-            type: DataTypes.STRING,
-            unique: true,
-            isEmail: true,
-            allowNull: false
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        token: {
-            type: DataTypes.VIRTUAL,
-            get: function () {
-                return jwt.sign({
-                    userName: this.userName
-                }, process.env.JWT_SECRET)
-            },
-            set(tokenObj) {
-                return jwt.sign(tokenObj, process.env.JWT_SECRET)
-            }
+  const User = sequelize.define("user", {
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      isEmail: true,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    token: {
+      type: DataTypes.VIRTUAL,
+      get: function () {
+        return jwt.sign(
+          {
+            userName: this.userName,
+          },
+          process.env.JWT_SECRET
+        );
+      },
+      set(tokenObj) {
+        return jwt.sign(tokenObj, process.env.JWT_SECRET);
+      },
+    },
+    role: {
+      type: DataTypes.ENUM,
+      values: ["admin", "user"],
+      allowNull: false,
+      defaultValue: "user",
+    },
+    capabilities: {
+      type: DataTypes.VIRTUAL,
+      get: function () {
+        const acl = {
+          admin: ["read", "create", "delete", "update"],
+          user: ["read", "create"],
         }
-    });
-    User.authenticateToken = token => {
-        // console.log(token, "from models");
-        return jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-            if(err){
-                return err;
-            } else {
-                return decoded;
-            }
-        })
+        return acl[this.role]
+      }
     }
-    return User;
-}
+    
+  },
+  {
+    freezeTableName: true,
+  }
+  
+  );
+  User.authenticateToken = (token) => {
+    // console.log(token, "from models");
+    return jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return err;
+      } else {
+        return decoded;
+      }
+    });
+  };
+  return User;
+};
